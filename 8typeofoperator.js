f832d13a5305290aa60795df69c053f7fe030220
maazi-git <maazi5172@gmail.com>
2024-09-19 18:18:25 +0500

    lecture 8 typeof operator

--- 8typeofoperator.js
+++ 8typeofoperator.js
@@ -1,7 +1,48 @@
-// type of operator
+// typeof operator
+// typeof operator tells us about the data type 
+
+
+// NOTE: We have data types in javascript for example  PRIMITIVE Data Types
+//  string "maazi" or 
+//  numbers like 2 ,5.8  etc 
+//  Booleans 
+//  undefined
+//  null 
+//  bigInt
+// symbol 
+
+    // use of typeof operator
+
+    let age =22;
+    let myname="Maazi"
+
+    console.log(typeof age);
+
+     console.log(typeof myname); 
+    //   u can also write the string value
+    //  console.log(typeof "Maazi");
 
 
 // convert number to string 
+                // way 1
+age= String(age);
+console.log(typeof age)
+
+                // way 2
+// if you want to convet your data type from number to string then simply add the string with it !
+// age = 22 + "";
+// console.log(typeof age);
+
+
 
 
 // convert string to number 
+                // way 1
+myname= Number(myname);
+console.log(typeof myname)
+
+
+                // way 2
+// from string to number you have to add simply + before the string . 
+// myname= +"myname";
+// console.log(typeof myname)
