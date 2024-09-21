02dfa4b02529b2171748d0147968df4744c9b52a
maazi-git <maazi5172@gmail.com>
2024-09-21 10:49:24 +0500

    new files of javaascript

--- 18-if,if,else-else.js
+++ 18-if,if,else-else.js
@@ -6,41 +6,63 @@
 
 
 
-let temperature = 25;
+// let temperature = 25;
 
-if (temperature > 45){
-    console.log("it's too hot!")
-}else if(temperature > 35){
-    console.log("little bit hot outside")
-}else if(temperature > 25){
-    console.log("summer is coming")
-}else if(temperature > 15){
-    console.log("lovely weather outside")
-}else if(temperature > 5){
-    console.log("it's too cold")
-}else{
-    console.log("I'm freezed")
-}
+// if (temperature > 45){
+//     console.log("it's too hot!")
+// }else if(temperature > 35){
+//     console.log("little bit hot outside")
+// }else if(temperature > 25){
+//     console.log("summer is coming")
+// }else if(temperature > 15){
+//     console.log("lovely weather outside")
+// }else if(temperature > 5){
+//     console.log("it's too cold")
+// }else{
+//     console.log("I'm freezed")
+// }
 
 
                 // Game Selectors 
 
-let age = +prompt("Write your age plz")
+// let age = +prompt("Write your age plz")
 
-if(age >= 18){
-    console.log("You can play PUBG, GTA 5, Forza Horizon!")
-}else if(age > 16){
-    console.log("You can play free fire and Asphalt 8")
-}else if(age > 14){
-    console.log("You can play traffic rider or 8 ball pool!")
-}else if(age > 12){
-    console.log("You can play Temple Run 3 or Shadow Ninja 2!")
-}else if(age > 10){
-    console.log("You can play Subway Surfers or Racing fever")
-}else if(age > 8){
-    console.log("You can play talking tom and bella!")
-}else{
-    console.log("You might watch t.v or youtube!")
+// if(age >= 18){
+//     console.log("You can play PUBG, GTA 5, Forza Horizon!")
+// }else if(age > 16){
+//     console.log("You can play free fire and Asphalt 8")
+// }else if(age > 14){
+//     console.log("You can play traffic rider or 8 ball pool!")
+// }else if(age > 12){
+//     console.log("You can play Temple Run 3 or Shadow Ninja 2!")
+// }else if(age > 10){
+//     console.log("You can play Subway Surfers or Racing fever")
+// }else if(age > 8){
+//     console.log("You can play talking tom and bella!")
+// }else{
+//     console.log("You might watch t.v or youtube!")
+// }
+// console.log("enjoy your game plz")
+
+
+            // Day Teller program 
+let day = +prompt("day number please!");
+
+if(day === 0){
+    console.log("Sunday");
+}else if(day === 1){
+    console.log("Monday")
+}else if(day === 2){
+    console.log("Tuesday")
+}else if(day === 3){
+    console.log("Wednesday")
+}else if(day === 4){
+    console.log("Thursday")
 }
-console.log("enjoy your game plz")
-
+else if(day === 5){
+    console.log("Friday")
+}else if(day === 6){
+    console.log("Saturday")
+}else{
+    console.log("invalid day")
+}
---
+++ 20-21-while-loop+example.js
@@ -0,0 +1,49 @@
+
+        // While Loop or its example
+// So, we have to print numbers from 0 to 9, 
+
+// i++  is increment
+// i-- is decrement 
+// the coding principle, don't repeat yourself
+
+// let i = 0;
+// console.log(i)
+// i++;
+// console.log(i)
+// i++;
+// console.log(i)
+// i++;
+// console.log(i)
+// i++;
+// console.log(i)
+
+
+// we dont need to write the same code again and again~
+// While loop is used for this purpose.
+
+// i = a=1;
+
+// while(i=9){
+//     console.log(i);
+//     i++;
+// }
+// console.log(`The current value of i is ${i}.`)
+
+
+                    // while loop example!
+
+    // // formula of nutural numbers 
+    // let num= 100;
+    // // (num*(num+1))/2;
+    // total = (num*(num+1))/2;
+    // console.log(total);
+    console.log("hii")
+
+let total = 0;
+i = 0;
+
+while(i>=10){
+    total= total + i;
+    i++;
+}
+console.log(total);
