250b775800a507e901d917752c40eb6a06b3cbe2
maazi-git <maazi5172@gmail.com>
2024-10-05 11:25:05 +0500

    new files attached, about objects literals

--- 41-Objects-destructuring.js
+++ 41-Objects-destructuring.js
@@ -1,1 +1,16 @@
 //              Object Destructuring 
+const band = {
+    bandname : "Jackson",
+    Song : "Unknown",
+    year : 1978
+}
+console.log(band)
+
+let {bandname:bandtag, Song:album, ...thisyear} = band;
+//      var     var       array 
+// this will make the two variables and an array, 
+// : colon is used to rename variable colin:comma,
+// ...thisyear is a new object, which will store the remaining keys or properties
+thisyear.year = 1233;
+console.log(bandtag);
+console.log(thisyear);
---
+++ 42-Objects-inside-Array.js
@@ -0,0 +1,34 @@
+//              Objects inside an array 
+//     this is very useful in real world applications.
+
+
+// So, we have to store the data of the many users!
+
+let Users = [
+    {UserID : 1, Fullname : "Maaz Haider", Gender : "Male"},
+    {UserID : 2, Fullname : "Abdul Shukran", Gender : "Male"},
+    {UserID : 3, Fullname : "Zain Malik", Gender : "Male"},
+    {UserID : 4, Fullname : "Zoha Malik", Gender : "Female"},
+    {UserID : 5, Fullname : "Ahtisham Nasir", Gender : "Male"},
+]
+
+// console.log(Users);
+for(let user of Users){
+    console.log(user.Fullname)
+}
+
+for (let users of Users){
+    console.log(users.UserID)
+}
+
+for(let user=0; user<Users.length; user++){
+    console.log(Users[user].UserID);
+}
+
+for (let i=0; i<Users.length; i++){
+    console.log(Users[i].Fullname);
+}
+
+for (let n=0; n<Users.length; n++){
+    console.log(Users[n].Gender)
+}
